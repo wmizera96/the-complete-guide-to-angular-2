3555b1dbbc3b6899ddc9ec574cb7c0c56b8c636b
wmizera96 <wmizera@outlook.com>
2023-08-02 17:07:45 +0200

    210

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-import { FormGroup } from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  getHobbyControls(){
+    return (<FormArray>this.signupForm.get('hobbies')).controls
+  }
 }
